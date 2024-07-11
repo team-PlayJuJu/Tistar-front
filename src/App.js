58fc9a510049de2516362edf1f686fa5a20b42af
pureyounhoo0 <younhoo070603@naver.com>
2024-07-11 21:00:01 +0900

    Login Finish

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/SignUp";
+import './assets/global.css'
 
 function App() {
   return (
